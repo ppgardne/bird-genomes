5563b9a5ac3c421cdc0d808900898a81f5d9e5c7
Paul Gardner <ppgardner@gmail.com>
2013-07-16 16:27:17 +1200

    Updated gffs2heatmaps.pl. Added overlaps, conserved-merged-annotations & misc accessory files.

--- scripts/gffs2heatmaps.pl
+++ scripts/gffs2heatmaps.pl
@@ -45,11 +45,13 @@ if (-s "$rDir/allRNA.dat"){
     while(my $w=<W>){
 	chomp($w);
 	my @w=split(/\t/, $w);
-	$whitelist{$w[50]}=1;
+	my $w=pop(@w);
+	$whitelist{$w}=1;
     }
     close(W);
 }
 
+#replace with a family2type hash:
 my %lncRNAlist;
 if (-s "data/rfam11_lncRNAs.txt"){
     open(W, "< data/rfam11_lncRNAs.txt");    
@@ -68,183 +70,75 @@ print "Reading GFFs\n" if (defined($verbose));
 my (%HoHspeciesRfamCount, %familiesSpeciesCounts, %familiesTotalCounts, %statistics, @bitscores, %tRNA, %tRNAfamilies); 
 foreach my $f (@gffs){
 
-    #print "$f\n";
+    print "$f\n" if(defined($verbose));
     my $species;
     if ($f=~/$gffsDir\/(\S+?_\S+?)\.gff/){
 	$species=$1; 
     }
+    else{
+	print "WARNING: no species name parsed from [$f], skipping!\n";
+	next;
+    }
     
     open(GFF, "< $f");
+    open(OUT, "> $outDir/$species\.gff");
     while(my $g=<GFF>){
 	chomp($g);
 	my @g=split(/\t/, $g);
-	my $type; 
-	if($g[8]=~/rfam-id=(\S+);evalue/ or $g[8]=~/Alias=(\S+);Note/){
-	    $type=$1;
-	    #push(@bitscores, $g[5]) if (defined($whitelist{$type}) && $g[5]=~/^\d+\.\d+$/); 
+	my $family; 
+	if($g[8]=~/rfam-id=(\S+);evalue/ or $g[8]=~/Alias=(\S+);Note/){#Rfam
+	    $family=$1;
+	    #push(@bitscores, $g[5]) if (defined($whitelist{$family}) && $g[5]=~/^\d+\.\d+$/); 
 	}
-	elsif($g[8]=~/ID=(\S+)\_\d+/){
-	    $type=$1;
+	elsif($g[8]=~/ID=(\S+)\_\d+/){#miRBase
+	    $family=$1;
 	}
-	elsif($g[8]=~/type=Pseudo;/){
-	    $type="tRNA-pseudogene";
+	elsif($g[8]=~/type=Pseudo;/){#tRNAscan
+	    $family="tRNA-pseudogene";
 	    $tRNA{$species}{"Pseudo"}=0 if(not defined($tRNA{$species}{"Pseudo"}));
 	    $tRNA{$species}{"Pseudo"}++;
 	    $tRNAfamilies{"Pseudo"}=0 if(not defined($tRNAfamilies{"Pseudo"}));
 	    $tRNAfamilies{"Pseudo"}++;
 	    #print "$g[8]\n";
 	}
-	elsif($g[8]=~/type=(\S+?);/){
-	    $type="tRNA";
+	elsif($g[8]=~/type=(\S+?);/){#tRNAscan
+	    $family="tRNA";
 	    $tRNA{$species}{$1}=0 if(not defined($tRNA{$species}{$1}));
 	    $tRNA{$species}{$1}++;
 	    $tRNAfamilies{$1}=0 if(not defined($tRNAfamilies{$1}));
 	    $tRNAfamilies{$1}++;
 	    #print "$g[8]\n";
 	}
-	else{
-	    $type=$g[2];
+	else{#WTF?
+	    $family=$g[2];
 	    #print "$g[2]\n";
 	}
-
-	print "$species\t$type\n" if defined($verbose);
-	if ( not defined($HoHspeciesRfamCount{$species}{$type}) ){
-	    $HoHspeciesRfamCount{$species}{$type}=0;
-	    $familiesSpeciesCounts{$type}=0 if (not defined($familiesSpeciesCounts{$type}));
-	    $familiesSpeciesCounts{$type}++;
+	
+	print "$species\t$family\n" if defined($verbose);
+	if ( not defined($HoHspeciesRfamCount{$species}{$family}) ){
+	    $HoHspeciesRfamCount{$species}{$family}=0;
+	    $familiesSpeciesCounts{$family}=0 if (not defined($familiesSpeciesCounts{$family}));
+	    $familiesSpeciesCounts{$family}++;
 	}
-	$familiesTotalCounts{$type}=0 if (not defined($familiesTotalCounts{$type}));
-	$familiesTotalCounts{$type}++;
-	$HoHspeciesRfamCount{$species}{$type}++;
+	$familiesTotalCounts{$family}=0 if (not defined($familiesTotalCounts{$family}));
+	$familiesTotalCounts{$family}++;
+	$HoHspeciesRfamCount{$species}{$family}++;
+	print OUT "$g\n" if defined($whitelist{$family});
     }
     close(GFF);
-    #my $stat = Statistics::Descriptive::Full->new();
-    #$stat->add_data(@bitscores);
-#    print "[@bitscores]\n";
-#    exit(0);
-    #$statistics{$species} = sprintf "%0.2f\t%0.2f\t%0.2f\n", $stat->quantile(1), $stat->median(), $stat->quantile(3);
-    #$stat->clear();
-    #@bitscores=();
+    close(OUT);
 }
 
-#ls clans_competed/*gff | perl -lane 'if (/\/(\S+?_\S+?)\-/){print "$1"}'
-#WHERE IS ANTROSTOMUS (CHUCK WILLS WIDOW) & MELOPSITTACUS (BUDGIE)?!!!
-#THIS IS CALLED Caprimugus_Carolinensis BELOW FOR SOME REASON?!
-#####renamed to Antrostomus_carolinensis
+#slurp species in phylogenetic order:
 my (@speciesPhyloOrder,@speciesCommonPhyloOrder); 
-
 open(SPC, "< data/species_list-phyloOrder.txt");
 while(my $s=<SPC>){
-    
+    if($s=~/^(\S+)\t(\S+)/){
+	push(@speciesPhyloOrder,$1); 
+	push(@speciesCommonPhyloOrder,$2); 
+    }
 }
 close(SPC); 
-
-# = qw(
-# Homo_sapiens
-# Alligator_mississippiensis
-# Chelonia_mydas
-# Struthio_camelus
-# Tinamus_guttatus
-# Anas_platyrhynchos
-# Meleagris_gallopavo
-# Gallus_gallus
-# Antrostomus_carolinensis
-# Chaetura_pelagica
-# Calypte_anna
-# Columba_livia
-# Cuculus_canorus
-# Mesitornis_unicolor
-# Pterocles_guturalis
-# Tauraco_erythrolophus
-# Chlamydotis_undulata
-# Opisthocomus_hoazin
-# Charadrius_vociferus
-# Balearica_regulorum
-# Podiceps_cristatus
-# Phoenicopterus_ruber
-# Eurypyga_helias
-# Phaethon_lepturus
-# Gavia_stellata
-# Egretta_garzetta
-# Nipponia_nippon
-# Pelecanus_crispus
-# Phalacrocorax_carbo
-# Fulmarus_glacialis
-# Pygoscelis_adeliae
-# Aptenodytes_forsteri
-# Cariama_cristata
-# Tyto_alba
-# Cathartes_aura
-# Haliaeetus_albicilla
-# Haliaeetus_leucocephalus
-# Colius_striatus
-# Leptosomus_discolor
-# Apaloderma_vittatum
-# Buceros_rhinoceros
-# Merops_nubicus
-# Picoides_pubescens
-# Falco_peregrinus
-# Nestor_notabilis
-# Melopsittacus_undulatus
-# Acanthisitta_chloris
-# Manacus_vitellinus
-# Corvus_brachyrhynchos
-# Taeniopygia_guttata
-# Geospiza_fortis
-# );
-#  = qw(
-# human
-# alligator
-# turtle
-# ostrich_85X
-# tinamou_100X
-# duck_50X
-# turkey
-# chicken
-# nightjar_30X
-# swift_106X
-# hummingbird_110X
-# pigeon_64X
-# cuckoo_100X
-# mesite_29X
-# sandgrouse_25X
-# turaco_30X
-# bustard_27X
-# hoatzin_100X
-# plover_100X
-# crane_33X
-# grebe_30X
-# flamingo_33X
-# sunbittern_33X
-# tropicbird_39X
-# loon_33X
-# egret_74X
-# ibis_105X
-# pelican_34X
-# cormorant_24X
-# fulmar_33X
-# adelie_penquin_60X
-# emperor_penquin_60X
-# seriema_24X
-# owl_27X
-# vulture_25X
-# white_tailed_eagle_26X
-# bald_eagle
-# mousebird_27X
-# cuckoo_roller_32X
-# trogon_28X
-# hornbill_35X
-# bee_eater_37X
-# woodpecker_105X
-# falcon_105X
-# kea_32X
-# budgerigar
-# rifleman_29X
-# manakin_110X
-# crow_90X
-# zebrafinch
-# ground_finch_115X
-# );
 
 print "Printing R-dat headers\n" if (defined($verbose));
 foreach my $ut (("$rDir/snoRNA.dat", "$rDir/miRNA.dat", "$rDir/RNA.dat", "$rDir/allRNA.dat", "$rDir/tRNA.dat", "$rDir/lncRNA.dat")){
@@ -260,8 +154,6 @@ foreach my $ut (("$rDir/snoRNA.dat", "$rDir/miRNA.dat", "$rDir/RNA.dat", "$rDir/
 }
 
 my $cnt=0;
-#my @family = ( sort {lc $a cmp lc $b} keys %{ $HoHspeciesRfamCount{$speciesPhyloOrder[0]} } );
-#my @family = ( sort &sortFamilies (keys %{ $HoHspeciesRfamCount{$speciesPhyloOrder[0]} } ) );
 
 print "Sorting family IDs\n" if (defined($verbose));
 my @family = sort {
@@ -329,7 +221,7 @@ foreach my $family ( @family ) {
     
     #print "$family\n";# if ($family=~/Pseudo/i);
     
-    #Family must be found in >50% of all species
+    #Family must be found in >10% of all species
     #printf "$family: $familiesSpeciesCounts{$family}/%d %0.2f\n", scalar(@speciesPhyloOrder), $familiesSpeciesCounts{$family}/scalar(@family) if($family=~/RNase_MRP/);
     next if( ($familiesSpeciesCounts{$family}/scalar(@speciesPhyloOrder)) < 0.1);
     next if(defined($blacklist{$family}));
@@ -386,6 +278,7 @@ print "System calls: creating [$rDir/snoRNA-human-yeast-correspondences.dat] & r
 
 #egrep '^snoR38;|SNORA13;|SNORA16;|SNORA2;|SNORA21;|SNORA26;|SNORA27;|SNORA28;|SNORA3;|SNORA36;|SNORA4;|SNORA44;|SNORA48;|SNORA5;|SNORA50;|SNORA52;|SNORA56;|SNORA58;|SNORA62;|SNORA64;|SNORA65;|SNORA66;|SNORA69;|SNORA7;|SNORA74;|SNORA76;|SNORA8;|SNORA9;|SNORD12;|SNORD14;|SNORD15;|SNORD16;|SNORD17;|SNORD18;|SNORD2;|SNORD24;|SNORD27;|SNORD29;|SNORD31;|SNORD33;|SNORD34;|SNORD35;|SNORD36;|SNORD38;|SNORD41;|SNORD43;|SNORD46;|SNORD51;|SNORD52;|SNORD57;|SNORD59;|SNORD60;|SNORD62;|SNORD65;|SNORD74;|SNORD77;|SNORD88;|SNORND104;|snosnR60_Z15' clans_competed/*gff | perl -lane 'if(/\/(\S+?)\-.*gff:(\S+).*\-id=(\S+);eval/ or /\/(\S+?)\-.*gff:(\S+).*Alias=(\S+);Not/){print "$1\t$2\t$3\t$F[6]"}'
 #GAS5 snoRNAs: \|SNORD81\$\|SNORD47\$\|SNORD80\$\|SNORD79\$\|SNORD78\$\|SNORD44\$\|SNORD77\$\|SNORD76\$\|SNORD75\$\|SNORD74\$
+#DAMN THIS IS NASTY!
 system("egrep \47^human\|snoR38\$\|SNORA13\$\|SNORA16\$\|SNORA2\$\|SNORA21\$\|SNORA26\$\|SNORA27\$\|SNORA28\$\|SNORA3\$\|SNORA36\$\|SNORA4\$\|SNORA44\$\|SNORA48\$\|SNORA5\$\|SNORA50\$\|SNORA52\$\|SNORA56\$\|SNORA58\$\|SNORA62\$\|SNORA64\$\|SNORA65\$\|SNORA66\$\|SNORA69\$\|SNORA7\$\|SNORA74\$\|SNORA76\$\|SNORA8\$\|SNORA9\$\|SNORD12\$\|SNORD14\$\|SNORD15\$\|SNORD16\$\|SNORD17\$\|SNORD18\$\|SNORD2\$\|SNORD24\$\|SNORD27\$\|SNORD29\$\|SNORD31\$\|SNORD33\$\|SNORD34\$\|SNORD35\$\|SNORD36\$\|SNORD38\$\|SNORD41\$\|SNORD43\$\|SNORD46\$\|SNORD51\$\|SNORD52\$\|SNORD57\$\|SNORD59\$\|SNORD60\$\|SNORD62\$\|SNORD65\$\|SNORD74\$\|SNORD77\$\|SNORD88\$\|SNORND104\$\|snosnR60_Z15\$\47 $rDir/snoRNA.dat > $rDir/snoRNA-human-yeast-correspondences.dat");
 
 system("R CMD BATCH --no-save scripts/heatmaps.R");
